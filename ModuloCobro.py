5883b2594d329f526c792f29f669ca41c098957e
Ayrton Sab <ayrton_leo08@hotmail.com.ar>
2025-06-11 14:20:08 -0300

    cambios en cobro

--- ModuloCobro.py
+++ ModuloCobro.py
@@ -3,14 +3,19 @@ from Productos import productos
 
 class PantallaCobro(Frame):
     def __init__(self, master):
-        super().__init__(master, bg="#052d55", width=900, height=450)
+        # 1024x768
+        super().__init__(master, bg="#052d55", width=1024, height=768)
         self.grid_propagate(False)
         self.master = master
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        label_titulo_cobro = Label(self, text="Pantalla de Cobro", font=("Arial", 16), bg="#b5daff")
+         # --- Barra superior ---
+        top_frame = Frame(self, bg="lightblue", bd=2, relief="groove")
+        top_frame.pack(side="top", fill="x", pady=10, padx=10)
+
+        label_titulo_cobro = Label(top_frame, text="Pantalla de Cobro", font=("Arial", 16), bg="#b5daff")
         label_titulo_cobro.grid(row=0, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
 
         # Input para código
@@ -23,22 +28,27 @@ class PantallaCobro(Frame):
 
         # Lista visual
         self.lista = Listbox(self, width=40)
-        self.lista.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+        self.lista.grid(row=2, column=1, columnspan=2, padx=10, pady=5)
+
+        # Botón volver
+        self.boton_volver = Button(self, text="Volver", command=lambda: master.mostrar_frame("PantallaPrincipal"), bg="#b5f5ff")
+        self.boton_volver.grid(row=2, column=0, columnspan=2, padx=20, pady=(10,10), sticky="ew")
+
+        # Boton eliminar ultimo producto registrado
+        self.boton_eliminar = Button(self, text="Eliminar último producto", command=lambda: self.lista.delete(END), bg="#ffb5b5")
+        self.boton_eliminar.grid(row=3, column=0, padx=10, pady=5, sticky="ew")
 
         # Total a pagar
         self.label_total = Label(self, text="Total: $0.00", font=("Arial", 14), bg="#b5daff")
         self.label_total.grid(row=3, column=0, columnspan=2, pady=5, sticky="ew")
 
-        # Botones de pago
-        self.boton_efectivo = Button(self, text="Efectivo", command=lambda: self.finalizar_compra("efectivo"), bg="#b5ffb5")
-        self.boton_credito = Button(self, text="Tarjeta de Crédito", command=lambda: self.finalizar_compra("credito"), bg="#b5b5ff")
-        self.boton_debito = Button(self, text="Tarjeta de Débito", command=lambda: self.finalizar_compra("debito"), bg="#ffd6b5")
-        self.boton_efectivo.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
-        self.boton_credito.grid(row=4, column=1, padx=10, pady=5, sticky="ew")
-        self.boton_debito.grid(row=5, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+        # Boton de pago
+        self.boton_pagar = Button(self, text="Pagar", command=self.finalizar_compra, bg="#b5ffb5")
+        self.boton_pagar.grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
-        self.boton_volver = Button(self, text="Volver", command=lambda: master.mostrar_frame("PantallaPrincipal"), bg="#b5f5ff")
-        self.boton_volver.grid(row=6, column=0, columnspan=2, padx=20, pady=(10,10), sticky="ew")
+        
+
+        
 
         # Variables de control
         self.lista_productos = []
@@ -59,11 +69,11 @@ class PantallaCobro(Frame):
         else:
             messagebox.showwarning("Código inválido", "El código no existe.")
 
-    def finalizar_compra(self, metodo):
+    def finalizar_compra(self):
         if not self.lista_productos:
             messagebox.showwarning("Atención", "Debe agregar al menos un producto.")
             return
-        messagebox.showinfo("Compra finalizada", f"Compra realizada por ${self.total:.2f} con {metodo}.")
+        messagebox.showinfo("Compra finalizada", f"Compra realizada por ${self.total:.2f}.")
         self.lista.delete(0, END)
         self.lista_productos.clear()
         self.total = 0.0
