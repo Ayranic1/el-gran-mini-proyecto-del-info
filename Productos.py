3686724db6404ccb98cc3b63fe5b8315eef821b3
Maxi-ram <maxi.ramirez508@gmail.comgit config user.name Maxi-ramgit config user.email maxi.ramirez508@gmail.com>
2025-06-29 02:38:45 -0300

    Se cambio imagen de la pagina principal se se agrego fecha y hora

--- Productos.py
+++ Productos.py
@@ -1,12 +1,6 @@
 # Lista de productos de ejemplo 
 # 
 productos = [
-    {"nombre": "Pan", "precio": 2000.00},
-    {"nombre": "Leche", "precio": 1800.00},
-    {"nombre": "Huevos", "precio": 4000.00},
-    {"nombre": "Carne", "precio": 15000.00},
-    {"nombre": "Arroz", "precio": 3000.00}
-    ]
     {"codigoProd": 123,"nombre": "Pan", "precio": 2000.00},
     {"codigoProd": 124,"nombre": "Leche", "precio": 1800.00},
     {"codigoProd": 125,"nombre": "Huevos", "precio": 4000.00},
